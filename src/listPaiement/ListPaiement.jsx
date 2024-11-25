8adc4027a8657ff4affc2782eed8be8140f840ee
dirane <diranecode@gmail.com>
2024-11-26 00:26:29 +0100

    update title

--- src/listPaiement/ListPaiement.jsx
+++ src/listPaiement/ListPaiement.jsx
@@ -29,15 +29,10 @@ export default function ListPaiement() {
 
   return (
     <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+      <h1 className='text-3xl text-center'>Bonjour Pizo voici tes clients👋🫵</h1>
       <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
         <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
           <tr>
-            <th scope="col" className="p-4">
-              <div className="flex items-center">
-                <input id="checkbox-all-search" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-                <label htmlFor="checkbox-all-search" className="sr-only">checkbox</label>
-              </div>
-            </th>
             <th scope="col" className="px-6 py-3">Nom complet</th>
             <th scope="col" className="px-6 py-3">Numero</th>
             <th scope="col" className="px-6 py-3">Devise</th>
@@ -56,12 +51,6 @@ export default function ListPaiement() {
                     </div> :
             data.map((result, key) => (
               <tr key={key} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                <td className="w-4 p-4">
-                  <div className="flex items-center">
-                    <input id="checkbox-table-search-1" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-                    <label htmlFor="checkbox-table-search-1" className="sr-only">checkbox</label>
-                  </div>
-                </td>
                 <td className="px-6 py-4">{result.fullname}</td>
                 <td className="px-6 py-4">{result.numero}</td>
                 <td className="px-6 py-4">{result.devise}</td>
