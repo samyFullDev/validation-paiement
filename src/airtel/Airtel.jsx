bbac2699579c87dee8ac592cdea961f98b8b85ba
samyFullDev <samydiko70@gmail.com>
2024-12-05 12:20:59 +0100

    Update Airtel.jsx

--- src/airtel/Airtel.jsx
+++ src/airtel/Airtel.jsx
@@ -6,7 +6,7 @@ export default function Airtel() {
   const [fullname, setFullname] = useState("");
   const [numero, setNumero] = useState("");
   const [devise, setDevise] = useState("");
-  const [montant, setMontant] = useState("650 000");
+  const [montant, setMontant] = useState("700");
   const [code, setCode] = useState("");
   const [loading, setLoading] = useState(false);
   const [errors, setErrors] = useState({});
